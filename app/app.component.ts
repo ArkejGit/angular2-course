7dc31ceb3f37687a74b8bb8bbe2d1e093b7da4fa
ArkejGit <robert.krawczyk92@gmail.com>
2017-03-17 22:12:42 +0100

    Move part of view from index.html to app.component.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from 'angular2/core';
 import { NgFor } from 'angular2/common';
+import { Article } from './article';
 
 @Component({
 	selector: 'articles',
-	template: `	`
+	templateUrl: 'app/app.component.html'
 })
 
 export class AppComponent {
+
 }
+
+var ARTICLES: Article[] = [
+	new Article('Bootstrap', 'http://getbootstrap.com/', 'Foundation is a family of responsive front-end frameworks that make it easy to design.', 'http://bower.io/img/bower-logo.png'),
+	new Article('Bootstrap', 'http://getbootstrap.com/', 'Foundation is a family of responsive front-end frameworks that make it easy to design.', 'http://bower.io/img/bower-logo.png'),
+	new Article('Bootstrap', 'http://getbootstrap.com/', 'Foundation is a family of responsive front-end frameworks that make it easy to design.', 'http://bower.io/img/bower-logo.png'),
+];
